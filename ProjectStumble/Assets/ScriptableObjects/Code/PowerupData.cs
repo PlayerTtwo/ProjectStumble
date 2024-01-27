2133cf177f1ec515cfa235d7712baee4f57e4e4a
aledinra <leon.nuredini@treedev.io>
2024-01-27 23:30:58 +0100

    minor changes

--- ProjectStumble/Assets/ScriptableObjects/Code/PowerupData.cs
+++ ProjectStumble/Assets/ScriptableObjects/Code/PowerupData.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PowerupData", menuName = "ScriptableObjects/PowerupData", order = 1)]
 public class PowerupData : ScriptableObject
 {
+    [SerializeField] private Sprite _iconSprite;
     [SerializeField] private PowerupType _powerUpType;
 
     public PowerupType PowerupType => _powerUpType;
+    public Sprite IconSprite => _iconSprite;
 }
