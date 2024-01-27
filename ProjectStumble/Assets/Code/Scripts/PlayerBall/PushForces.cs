a159705a97fde6abdb2ec84315cd6fbb12e8a048
aledinra <leon.nuredini@treedev.io>
2024-01-27 17:18:13 +0100

    Added push forces script

---
+++ ProjectStumble/Assets/Code/Scripts/PlayerBall/PushForces.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushForces : MonoBehaviour
+{
+    [SerializeField] private float _power = 10f;
+    [SerializeField] private float _radius = 2f;
+
+    private Rigidbody _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponentInParent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        _rigidBody.AddExplosionForce(_power, transform.position, _radius, 3.0F);
+    }
+}
